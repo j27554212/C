578d4000d6c734a10272140f9d82580000b924ad
j27554212 <90737564+j27554212@users.noreply.github.com>
2021-11-04 22:01:17 +0800

    Create 圓.c

---
+++ 練習內容/圓.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+ 
+
+int main(){
+
+    float pi=3.14159;
+	double r = 6.4 ;
+	printf("圓的半徑為%f,圓面積為%f\n",r,pi*r*r);
+	printf("圓的半徑為%f,圓周長為%f\n",r,pi*r*2);
+	printf("圓的半徑為%f,1/4圓周長為%f\n",r,pi*r*r/4);	
+
+system("PAUSE");
+return 0 ;	
+	
+	
+	
+} 
