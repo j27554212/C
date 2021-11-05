75c99f7907fe599db9722537f49153be86643008
j27554212 <90737564+j27554212@users.noreply.github.com>
2021-11-05 10:19:11 +0800

    Update if...else.c

--- 練習內容/if...else.c
+++ 練習內容/if...else.c
@@ -7,9 +7,9 @@ int main(){
   printf("請輸入密碼");
   scanf("%s\n",pw);
   printf("輸入的密碼為%s\n",pw);
-  if(strcmp(pw,"5875487")==0)
-  	printf("\n輸入正確\n\n");
-	  else
+     if(strcmp(pw,"5875487")==0)
+  	   printf("\n輸入正確\n\n");
+	else
 	  printf("\n輸入失敗\n\n");
 	  
 	   
