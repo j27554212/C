bac3967a3903ca7654879de0a754eaa4ff60929d
j27554212 <90737564+j27554212@users.noreply.github.com>
2021-11-26 10:16:37 +0800

    Update switch.c

--- 上課內容/switch.c
+++ 上課內容/switch.c
@@ -20,32 +20,33 @@ int main(){
 	   	
 	   	case 'a' :
 		   printf("score 90 to  100. \n");
-		   break;
-		case 'b' :
-		   printf("score 80 to  90. \n");
-		   break; 
-   	   	case 'c' :
-		   printf("score 70 to  80. \n");
-		   break; 
-		case 'd' :
-		   printf("score 60 to  70. \n");
-		   break; 
-		case 'e' :
+		   //break; 
+	  	case 'b' :
+		   printf("score 80 to  89. \n");
+		   //break; 
+  	   	case 'c' :
+		   printf("score 70 to  79. \n");
+		   //break; 
+	   	case 'd' :
+		   printf("score 60 to  69. \n");
+		   //break; 
+	   	case 'e' :
 		   printf("score 0 to  59. \n");
-		   break; 
+		   //break; 
 	   } 
-	  
-	        if(grade == 'a'){
+	   
+	         if(grade == 'a'){
 	   	   printf("score 90 to  100. \n");
 	   	}else if (grade ==  'b'){
-	   	   printf("score 80 to  90. \n");
+	   	   printf("score 80 to  89. \n");
 	  	}else if (grade ==  'c'){
-	  	   printf("score 70 to  80. \n");
+	  	   printf("score 70 to  79. \n");
                 }else if (grade ==  'd'){ 
-		   printf("score 60 to  70. \n");
+		   printf("score 60 to  69. \n");
 		}else if (grade ==  'e'){
 		   printf("score 0 to  59. \n");
-		}
+
+		   }
 	
 	
 	
